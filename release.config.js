d1a1efab8c05a8d67882890411614384d43681ac
Rafał Maciejewski <rafaljestsuper@gmail.com>
2020-02-28 12:45:35 +0100

    fix: remove useless files from published package, add @semantic-release/git plugin

--- release.config.js
+++ release.config.js
@@ -1,8 +1,10 @@
 module.exports = {
+    branches: ['master'],
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
         '@semantic-release/changelog',
+        '@semantic-release/git',
         '@semantic-release/npm',
         '@semantic-release/github',
     ],
