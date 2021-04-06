e864a1057891462f981c86e7869bd3a9dcd459e7
Rafał Maciejewski <rafaljestsuper@gmail.com>
2021-04-06 15:52:19 +0200

    style: reformat code with prettier

--- index.js
+++ index.js
@@ -1,59 +1,59 @@
 module.exports = {
-    root: true,
-    parser: 'babel-eslint',
-    extends: [
-        './rules/best-practices',
-        './rules/errors',
-        './rules/es6',
-        './rules/jest',
-        './rules/node',
-        './rules/style',
-        './rules/variables',
-        './rules/typescript',
-    ].map(require.resolve),
-    parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: 'module',
-        ecmaFeatures: {
-            jsx: true,
-            legacyDecorators: true,
-        },
+  root: true,
+  parser: 'babel-eslint',
+  extends: [
+    './rules/best-practices',
+    './rules/errors',
+    './rules/es6',
+    './rules/jest',
+    './rules/node',
+    './rules/style',
+    './rules/variables',
+    './rules/typescript',
+  ].map(require.resolve),
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+      legacyDecorators: true,
     },
-    rules: {
-        strict: ['off'],
+  },
+  rules: {
+    strict: ['off'],
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        lib: ['ESNext', 'DOM', 'DOM.Iterable', 'ES6', 'ES7', 'WebWorker', 'ScriptHost'],
+        warnOnUnsupportedTypeScriptVersion: false,
+      },
     },
-    overrides: [
-        {
-            files: ['*.ts', '*.tsx'],
-            parser: '@typescript-eslint/parser',
-            parserOptions: {
-                lib: ['ESNext', 'DOM', 'DOM.Iterable', 'ES6', 'ES7', 'WebWorker', 'ScriptHost'],
-                warnOnUnsupportedTypeScriptVersion: false,
-            },
-        },
-        {
-            files: [
-                '*.test.ts',
-                '*.test.tsx',
-                '**/__mocks__/**/*.ts',
-                '**/__mocks__/**/*.tsx',
-                '*.spec.ts',
-                '*.spec.tsx',
-                '*.test-e2e.ts',
-                '*.test-e2e.tsx',
-                '*.spec-e2e.ts',
-                '*.spec-e2e.tsx',
-            ],
-            rules: {
-                '@typescript-eslint/explicit-function-return-type': ['off'],
-                '@typescript-eslint/typedef': ['off'],
-                '@typescript-eslint/no-magic-numbers': ['off'],
-                '@typescript-eslint/no-empty-function': ['off'],
-                '@typescript-eslint/no-extraneous-class': ['off'],
-                '@typescript-eslint/no-explicit-any': ['off'],
-            },
-            parser: '@typescript-eslint/parser',
-        },
-    ],
-    ignorePatterns: ['node_modules/', 'coverage/', 'public/', 'dist/', 'build/', '**/__snapshots__'],
+    {
+      files: [
+        '*.test.ts',
+        '*.test.tsx',
+        '**/__mocks__/**/*.ts',
+        '**/__mocks__/**/*.tsx',
+        '*.spec.ts',
+        '*.spec.tsx',
+        '*.test-e2e.ts',
+        '*.test-e2e.tsx',
+        '*.spec-e2e.ts',
+        '*.spec-e2e.tsx',
+      ],
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': ['off'],
+        '@typescript-eslint/typedef': ['off'],
+        '@typescript-eslint/no-magic-numbers': ['off'],
+        '@typescript-eslint/no-empty-function': ['off'],
+        '@typescript-eslint/no-extraneous-class': ['off'],
+        '@typescript-eslint/no-explicit-any': ['off'],
+      },
+      parser: '@typescript-eslint/parser',
+    },
+  ],
+  ignorePatterns: ['node_modules/', 'coverage/', 'public/', 'dist/', 'build/', '**/__snapshots__'],
 };
--- rules/node.js
+++ rules/node.js
@@ -1,43 +1,43 @@
 module.exports = {
-    env: {
-        node: true,
-        commonjs: true,
-    },
-    rules: {
-        // enforce return after a callback
-        'callback-return': 'off',
+  env: {
+    node: true,
+    commonjs: true,
+  },
+  rules: {
+    // enforce return after a callback
+    'callback-return': 'off',
 
-        // require all requires be top-level
-        // http://eslint.org/docs/rules/global-require
-        'global-require': 'off',
+    // require all requires be top-level
+    // http://eslint.org/docs/rules/global-require
+    'global-require': 'off',
 
-        // enforces error handling in callbacks (node environment)
-        'handle-callback-err': 'off',
+    // enforces error handling in callbacks (node environment)
+    'handle-callback-err': 'off',
 
-        // disallow use of the Buffer() constructor
-        // http://eslint.org/docs/rules/no-buffer-constructor
-        'no-buffer-constructor': 'error',
+    // disallow use of the Buffer() constructor
+    // http://eslint.org/docs/rules/no-buffer-constructor
+    'no-buffer-constructor': 'error',
 
-        // disallow mixing regular variable and require declarations
-        'no-mixed-requires': ['off', false],
+    // disallow mixing regular variable and require declarations
+    'no-mixed-requires': ['off', false],
 
-        // disallow use of new operator with the require function
-        'no-new-require': 'error',
+    // disallow use of new operator with the require function
+    'no-new-require': 'error',
 
-        // disallow string concatenation with __dirname and __filename
-        // http://eslint.org/docs/rules/no-path-concat
-        'no-path-concat': 'error',
+    // disallow string concatenation with __dirname and __filename
+    // http://eslint.org/docs/rules/no-path-concat
+    'no-path-concat': 'error',
 
-        // disallow use of process.env
-        'no-process-env': 'off',
+    // disallow use of process.env
+    'no-process-env': 'off',
 
-        // disallow process.exit()
-        'no-process-exit': 'off',
+    // disallow process.exit()
+    'no-process-exit': 'off',
 
-        // restrict usage of specified node modules
-        'no-restricted-modules': 'off',
+    // restrict usage of specified node modules
+    'no-restricted-modules': 'off',
 
-        // disallow use of synchronous methods (off by default)
-        'no-sync': 'off',
-    },
+    // disallow use of synchronous methods (off by default)
+    'no-sync': 'off',
+  },
 };
